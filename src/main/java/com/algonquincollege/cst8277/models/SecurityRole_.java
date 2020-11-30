feb330040e9e5fd3ed5361f416b95107d831778b
Hanna Bernyk <bern0304@algonquinlive.com>
2020-11-29 23:02:58 -0500

    first commit

---
+++ src/main/java/com/algonquincollege/cst8277/models/SecurityRole_.java
@@ -0,0 +1,12 @@
+package com.algonquincollege.cst8277.models;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2020-11-29T22:40:45.021-0500")
+@StaticMetamodel(SecurityRole.class)
+public class SecurityRole_ {
+	public static volatile SingularAttribute<SecurityRole, Integer> id;
+	public static volatile SingularAttribute<SecurityRole, String> roleName;
+}
